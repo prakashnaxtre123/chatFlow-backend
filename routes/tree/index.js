bbfaee62d0254ec070223401d1826491c3339480
prakashnaxtre <prakash.naxtre@gmail.com>
2024-07-30 16:06:59 +0530

    delete api implemented

--- routes/tree/index.js
+++ routes/tree/index.js
@@ -10,7 +10,7 @@ router.post('/add',treeController.addTree,successResponse,unsuccessResponse)
  router.post('/get',treeController.getTree,successResponse,unsuccessResponse)
 /*router.put('/update/:id',postController.updatePost,successResponse,unsuccessResponse)
 
-router.get('/get/:id',postController.getPostById,successResponse,unsuccessResponse)
-router.delete('/delete/:id',postController.deletePostById,successResponse,unsuccessResponse) */
+router.get('/get/:id',postController.getPostById,successResponse,unsuccessResponse)*/
+router.delete('/delete/:id',treeController.deleteTreeById,successResponse,unsuccessResponse) 
 
 module.exports = router;
