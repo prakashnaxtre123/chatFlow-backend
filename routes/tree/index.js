65bd988ffb3fc864ec5f3462b8b2dfc685da61da
prakashnaxtre <prakash.naxtre@gmail.com>
2024-07-30 17:49:11 +0530

    update

--- routes/tree/index.js
+++ routes/tree/index.js
@@ -8,9 +8,9 @@ const unsuccessResponse = require('../../utils/unsuccessResponse');
 
 router.post('/add',treeController.addTree,successResponse,unsuccessResponse)
  router.post('/get',treeController.getTree,successResponse,unsuccessResponse)
-/*router.put('/update/:id',postController.updatePost,successResponse,unsuccessResponse)
+router.put('/update/:id',treeController.updateTree,successResponse,unsuccessResponse)
 
-router.get('/get/:id',postController.getPostById,successResponse,unsuccessResponse)*/
+router.get('/get/:id',treeController.getTreeById,successResponse,unsuccessResponse)
 router.delete('/delete/:id',treeController.deleteTreeById,successResponse,unsuccessResponse) 
 
 module.exports = router;
